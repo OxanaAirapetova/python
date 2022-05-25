f40c0545273da9bc4686b41699fc9347b41e5933
Oxana <linepro1995@mail.ru>
2022-05-25 15:49:43 +0300

    Урок 3, Задание 2

---
+++ DZ3/DZ_3_2G.py
@@ -0,0 +1,31 @@
+# * (вместо задачи 1) Доработать предыдущую функцию в num_translate_adv():
+# реализовать корректную работу с числительными, начинающимися с заглавной буквы —
+# результат тоже должен быть с заглавной. Например:
+# >>> num_translate_adv("One")
+# "Один"
+# >>> num_translate_adv("two")
+# "два"
+
+elem = {'zero': 'ноль',
+        'one': 'один',
+        'two': 'два',
+        'three': 'три',
+        'four': 'четыре',
+        'five': 'пять',
+        'six': 'шесть',
+        'seven': 'семь',
+        'eight': 'восемь',
+        'nine': 'девять',
+        'ten': 'десять'
+        }
+
+
+def num_translate_adv(num):
+    if num == num.capitalize():
+        return print(elem.get(num.lower()).capitalize())
+    else:
+        return print(elem.get(num))
+
+
+enter = input('Введите число на английском языке: ')
+num_translate_adv(enter)
